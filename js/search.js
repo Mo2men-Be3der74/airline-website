9cc7934a9ea89e93d2fcdd37ff60220fa0004a7b
marwanmuhmmed25122007 <marwanmuhmmed25122007@gmail.com>
2026-04-24 18:41:33 +0300

    Added search form row 2 and improved css styling, Prepared data array for countries

---
+++ js/search.js
@@ -0,0 +1,17 @@
+const Countries = [
+    "Egypt", "Oman" , "England", "Ethiopia",
+    "USA", "UAE", "Uganda",
+    "France", "Finland", "Fiji",
+    "Germany", "Greece", "Ghana",
+    "Italy", "Ireland", "India",
+    "Japan", "Jordan", "Jamaica",
+    "Canada", "China", "Croatia",
+    "Spain", "Sweden", "Switzerland",
+    "Morocco", "Mexico", "Malaysia",
+    "Turkey", "Thailand", "Tunisia",
+    "Brazil", "Belgium", "Bahrain",
+    "Portugal", "Poland",
+    "Netherlands", "Norway",
+    "Saudi Arabia", "Singapore", "Serbia",
+    "Qatar", "Kuwait"  
+];
