306a7e3278045ddf605f9c99441ea413b236b836
marwanmuhmmed25122007 <marwanmuhmmed25122007@gmail.com>
2026-06-08 04:05:11 +0300

    Update password visibility toggle icons in login and register pages

--- js/login_script.js
+++ js/login_script.js
@@ -111,14 +111,14 @@ window.togglePassword = function (inputId, iconId) {
     if (passInput.type === "password") {
         passInput.type = "text";
         if (icon) {
-            icon.classList.remove("fa-eye-slash");
-            icon.classList.add("fa-eye");
+            icon.classList.remove("fa-eye");
+            icon.classList.add("fa-eye-slash");
         }
     } else {
         passInput.type = "password";
         if (icon) {
-            icon.classList.remove("fa-eye");
-            icon.classList.add("fa-eye-slash");
+            icon.classList.remove("fa-eye-slash");
+            icon.classList.add("fa-eye");
         }
     }
 };
